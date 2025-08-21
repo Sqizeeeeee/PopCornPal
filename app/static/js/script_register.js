15097c405dc1e7dc9dbcf449d56f1a9ed06674e1
Vladimir <vladimir.oleynyk057@gmail.com>
2025-08-21 13:08:49 +0300

    added js files instead of script blocks

---
+++ app/static/js/script_register.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const flashes = document.querySelectorAll(".flash-messages li");
+    flashes.forEach(flash => {
+        setTimeout(() => {
+            flash.classList.add("hide");
+            setTimeout(() => flash.remove(), 500);
+        }, 3000);
+    });
+
+    const emojis = ["🎫", "🎰", "💰", "🏃‍♂️", "🎥", "🎬", "🍿", "🎞️", "🏴‍☠️", "🇺🇸", "👮‍♂️", "🚔", "🕵️‍♂️", "🚨", "💼", " 🚀", "✈️", "🍕", "☢️", "⁉️", "👑", "🗡️", "🎫", "🎰", "💰", "🏃‍♂️", "🎥", "🎬", "🍿", "🎞️", "🏴‍☠️", "🇺🇸", "👮‍♂️", "🚔", "🕵️‍♂️", "🚨", "💼", " 🚀", "✈️", "🍕", "☢️", "⁉️", "👑", "🗡️", "⭐️", "❄️", "🌊", "💧", "⚽️", "🌄", "🌃", "🔥", "🐬", "⭐️", "❄️", "🌊", "💧", "⚽️", "🌄", "🌃", "🔥", "🐬"];
+    const body = document.body;
+
+    emojis.forEach(emoji => {
+        const div = document.createElement("div");
+        div.classList.add("emoji-bg");
+        div.textContent = emoji;
+        div.style.left = Math.random() * 95 + "%";
+        div.style.fontSize = (20 + Math.random() * 30) + "px";
+        div.style.animationDuration = (8 + Math.random() * 8) + "s";
+        body.appendChild(div);
+    });
+});
